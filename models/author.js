c6fa8d6d0e0728d39055c938e9b68fc1b04a715b
mattsald48 <matt.saldana8@gmail.com>
2021-04-20 00:27:14 -0500

    corrected spelling error

--- models/author.js
+++ models/author.js
@@ -8,7 +8,8 @@ const authorSchema = new mongoose.Schema({
   },
 });
 
-//Wil run any function before the remove call is made
+//the pre() Wil run any function before the designated call is made. In this case the remove.
+//Stops you from deleting author who has books assigned to them
 authorSchema.pre('remove', function (next) {
   Book.find({ author: this.id }, (err, books) => {
     if (err) {
@@ -20,5 +21,17 @@ authorSchema.pre('remove', function (next) {
     }
   });
 });
+
+//Allows you to remove an author and all currently assigned books to them. May implement later.
+// authorSchema.pre('remove', function (next) {
+//   Book.find({ author: this.id }, (err, books) => {
+//     if (err) {
+//       next(err);
+//     } else {
+//       books.forEach((book) => book.remove());
+//     }
+//     next();
+//   });
+// });
 
 module.exports = mongoose.model('Author', authorSchema);
